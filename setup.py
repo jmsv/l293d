3966f07bc7fc77094f955c4bd1864b940f70d11c
James Vickery <jamesevickery.dev@gmail.com>
2016-10-23 21:26:12 +0000

    Fixed setup.py linting for #35

--- setup.py
+++ setup.py
@@ -3,21 +3,23 @@ from l293d import version
 
 try:
     import yaml
+    testyaml = yaml.load('name: value')
 except:
-    raise ImportError('Please install python-yaml (sudo apt-get install python-yaml)')
+    raise ImportError('Please install python-yaml '
+                      '(sudo apt-get install python-yaml)')
 
-from distutils.core import setup,Extension
+from distutils.core import setup
 
 setup(
-    name = 'l293d',
-    version = version.num,
-    author = 'James Vickery',
-    author_email = 'jamesevickery.dev@gmail.com',
-    description = ('A Python module to drive motors using an L293D via Raspberry Pi GPIO'),
-    license = 'MIT',
-    keywords = 'raspberry pi gpio l293d chip motor driver',
-    url = 'https://github.com/jamesevickery/l293d',
-    download_url = 'https://github.com/jamesevickery/l293d/tarball/master',
-    packages = ['l293d'],
-    install_requires = ['python-dev', 'python-pip', 'python-yaml']
+    name='l293d',
+    version=version.num,
+    author='James Vickery',
+    author_email='jamesevickery.dev@gmail.com',
+    description=('A Python module to drive motors using '
+                 'an L293D via Raspberry Pi GPIO'),
+    license='MIT',
+    keywords='raspberry pi gpio l293d chip motor driver',
+    url='https://github.com/jamesevickery/l293d',
+    download_url='https://github.com/jamesevickery/l293d/tarball/master',
+    packages=['l293d']
 )
