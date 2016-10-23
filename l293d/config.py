6bcf9714a105083fbd407f1f0204f8c98ead12e2
Patrick O'Connor <patoconnor43@gmail.com>
2016-10-23 14:34:35 -0700

    Fix linting warnings

--- l293d/config.py
+++ l293d/config.py
@@ -1,20 +1,20 @@
-config_path = '' # Custom config filepath
-#^ For example: '/home/arthurdent/configs/l293d.yaml'
-
-
 import os
+import yaml
+
+config_path = ''  # Custom config filepath
+# ^ For example: '/home/arthurdent/configs/l293d.yaml'
+
 # If no custom path, user's home directory is used
 if not config_path:
     # Get user home location and append filename.extension
     config_path = os.path.expanduser('~') + '/l293d-config.yaml'
-
 
 config_path_exists = os.path.exists(config_path)
 if (not config_path_exists):
     # Load default config
     default_file_path = str(os.path.realpath(__file__))
     # Get path of this file, and remove filename
-    while(default_file_path[-1] != '/'):
+    while default_file_path[-1] != '/':
         default_file_path = default_file_path[:-1]
     # Add default config filename
     default_file_path += 'default_l293d-config.yaml'
@@ -23,14 +23,12 @@ if (not config_path_exists):
     # Read file to config contents, then close
     config_contents = default_file.read()
     default_file.close()
-    
+
     # Write to user config at config path
     new_file = open(config_path, 'w')
     new_file.write(config_contents)
     new_file.close()
 
-
-import yaml
 # https://martin-thoma.com/configuration-files-in-python/#yaml
 # Open and load YAML config file
 try:
@@ -38,8 +36,8 @@ try:
     with open(config_path, 'r') as ymlfile:
         config = yaml.load(ymlfile)
 except:
-    raise IOError('Error loading config from: {}. Check config exists.'.format(config_path))
-
+    raise IOError('Error loading config from: {}. Check config exists.'.format(
+        config_path))
 
 # Assign loaded YAML to config variables
 # These are loaded outside of config.py as, e.g. config.verbose
