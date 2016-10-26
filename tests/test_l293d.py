a8a14a3c98783f74ebc574ed99b7dacfdd1459b9
the-zebulan <the-zebulan@users.noreply.github.com>
2016-10-26 17:24:41 -0600

    Class names should use CamelCase convention

--- tests/test_l293d.py
+++ tests/test_l293d.py
@@ -1,12 +1,12 @@
 import unittest
 
 
-class tests(unittest.TestCase):
-
+class L293DTestCase(unittest.TestCase):
     def test_import(self):
         import l293d.driver
         print(str(l293d.driver))
         self.assertTrue(True)
+
 
 if __name__ == '__main__':
     unittest.main()
