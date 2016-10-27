8cf81dd016fac81db641fb8e0ffb155a41f3ca21
James Vickery <jamesevickery.dev@gmail.com>
2016-10-27 00:21:32 +0000

    Added pins_string_list test, #46

--- tests/test_l293d.py
+++ tests/test_l293d.py
@@ -7,6 +7,10 @@ class L293DTestCase(unittest.TestCase):
         print(str(l293d.driver))
         self.assertTrue(True)
 
+    def test_pins_string_list(self):
+        import l293d.driver as d
+        motor = d.Motor(29, 7, 13)
+        self.assertEqual(motor.pins_string_list(), '[29, 7 and 13]')
 
 if __name__ == '__main__':
     unittest.main()
