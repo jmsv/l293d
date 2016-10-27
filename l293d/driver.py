2c13414dd74e5ab4dbe2a9fd04a75f40426bf5b7
James Vickery <jamesevickery.dev@gmail.com>
2016-10-27 22:58:32 +0000

    Tests for #46
    Added some tests to tests/test_l293d.py and moved pins_are_valid() out of the Motor class

--- l293d/driver.py
+++ l293d/driver.py
@@ -70,41 +70,13 @@ class Motor(object):
         self.motor_pins[2] = pin_c
 
         # Check pins are valid
-        self.pins_are_valid(self.motor_pins)
+        if pins_are_valid(self.motor_pins):
+            self.exists = True
         # Append to global list of pins in use
         for pin in self.motor_pins:
             pins_in_use.append(pin)
         # Set up GPIO mode for pins
         self.gpio_setup(self.motor_pins)
-
-    def pins_are_valid(self, pins, force_selection=False):
-        """
-        Check the pins specified are valid for pin numbering in use
-        """
-        global pin_numbering
-        if pin_numbering == 'BOARD':  # Set valid pins for BOARD
-            valid_pins = [
-                7, 11, 12, 13, 15, 16, 18, 22, 29, 31, 32, 33, 36, 37
-            ]
-        elif pin_numbering == 'BCM':  # Set valid pins for BCM
-            valid_pins = [
-                4, 5, 6, 12, 13, 16, 17, 18, 22, 23, 24, 25, 26, 27
-            ]
-        else:  # pin_numbering value invalid
-            raise ValueError("pin_numbering must be either 'BOARD' or 'BCM'.")
-        for pin in pins:
-            pin_int = int(pin)
-            if pin_int not in valid_pins and force_selection is False:
-                err_str = (
-                    "GPIO pin number must be from list of valid pins: %s"
-                    "\nTo use selected pins anyway, set force_selection=True "
-                    "in function call." % str(valid_pins))
-                raise ValueError(err_str)
-            if pin in pins_in_use:
-                raise ValueError('GPIO pin {} already in use.'.format(pin))
-        self.motor_pins = pins
-        self.exists = True
-        return True
 
     @staticmethod
     def gpio_setup(pins):
@@ -216,6 +188,34 @@ class Motor(object):
             raise ValueError('Motor has been removed. '
                              'If you wish to use this motor again, '
                              'you must redefine it.')
+
+
+def pins_are_valid(pins, force_selection=False):
+    """
+    Check the pins specified are valid for pin numbering in use
+    """
+    global pin_numbering
+    if pin_numbering == 'BOARD':  # Set valid pins for BOARD
+        valid_pins = [
+            7, 11, 12, 13, 15, 16, 18, 22, 29, 31, 32, 33, 36, 37
+        ]
+    elif pin_numbering == 'BCM':  # Set valid pins for BCM
+        valid_pins = [
+            4, 5, 6, 12, 13, 16, 17, 18, 22, 23, 24, 25, 26, 27
+        ]
+    else:  # pin_numbering value invalid
+        raise ValueError("pin_numbering must be either 'BOARD' or 'BCM'.")
+    for pin in pins:
+        pin_int = int(pin)
+        if pin_int not in valid_pins and force_selection is False:
+            err_str = (
+                "GPIO pin number must be from list of valid pins: %s"
+                "\nTo use selected pins anyway, set force_selection=True "
+                "in function call." % str(valid_pins))
+            raise ValueError(err_str)
+        if pin in pins_in_use:
+            raise ValueError('GPIO pin {} already in use.'.format(pin))
+    return True
 
 
 def cleanup():
--- tests/test_l293d.py
+++ tests/test_l293d.py
@@ -3,14 +3,49 @@ import unittest
 
 class L293DTestCase(unittest.TestCase):
     def test_import(self):
+        """
+        Import driver module, then assertTrue.
+        If the import fails, an exception is
+        raised and the assertion is never reached
+        """
         import l293d.driver
         print(str(l293d.driver))
         self.assertTrue(True)
+        reload(l293d.driver)  # Revert changes
 
     def test_pins_string_list(self):
+        """
+        Test that the list of pins is returned correctly
+        """
         import l293d.driver as d
+        print(d.pins_in_use)
         motor = d.Motor(29, 7, 13)
         self.assertEqual(motor.pins_string_list(), '[29, 7 and 13]')
+        reload(d)
+
+    def test_pins_are_valid_board_1(self):
+        """
+        Test for valid pins when all others are in use
+        """
+        import l293d.driver as d
+        d.pins_in_use = [7, 11, 12, 13, 15, 29, 31, 32, 33, 36, 37]
+        self.assertTrue(d.pins_are_valid([22, 18, 16]))
+        reload(d)
+
+    def test_pins_are_valid_board_2(self):
+        """
+        Test for valid pins when a pin is already in use
+        """
+        import l293d.driver as d
+        d.pins_in_use = [7, 11, 12]
+        try:
+            valid = d.pins_are_valid([31, 36, 11])
+            if valid:
+                self.assertFalse(True)
+        except:
+            self.assertFalse(False)
+        reload(d)
+
 
 if __name__ == '__main__':
     unittest.main()
