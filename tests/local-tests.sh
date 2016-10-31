63c100753f98701b4836fa646952bd902451bc90
James Vickery <jamesevickery.dev@gmail.com>
2016-10-31 17:03:49 +0000

    Script to run all checks
    Assumes that testing libraries etc. are installed

---
+++ tests/local-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# to be run as ./tests/local-tests.sh
+
+set -e
+
+# install
+echo "     -----------------   Installing l293d for Python 2.    -----------------"
+sudo python2 setup.py install
+echo "     -----------------   Installing l293d for Python 3.    -----------------"
+sudo python3 setup.py install
+
+# linting check
+flake8 .
+echo "     ----------------- flake 8 checks passed - returned 0. -----------------"
+
+# run tests
+py.test tests/
+python3 -m pytest tests/
+
+echo "     -----------------         All checks passed.          -----------------"
