1aa462810d97da58c4827ec0e650745bfe91b4d0
jamesevickery <jamesevickery.dev@gmail.com>
2017-05-24 03:55:14 +0100

    Inserted import statement as described in #59
    Modified .travis.yml to make flake8 ignore __init__.py

--- tests/local-tests.sh
+++ tests/local-tests.sh
@@ -5,17 +5,17 @@
 set -e
 
 # install
-echo "     -----------------   Installing l293d for Python 2.    -----------------"
+echo "   ------------   Installing l293d for Python 2.    ------------"
 sudo python2 setup.py install
-echo "     -----------------   Installing l293d for Python 3.    -----------------"
+echo "   ------------   Installing l293d for Python 3.    ------------"
 sudo python3 setup.py install
 
 # linting check
-flake8 .
-echo "     ----------------- flake 8 checks passed - returned 0. -----------------"
+flake8 . --exclude __init__.py
+echo "   ------------ flake 8 checks passed - returned 0. ------------"
 
 # run tests
 py.test tests/
 python3 -m pytest tests/
 
-echo "     -----------------         All checks passed.          -----------------"
+echo "   ------------         All checks passed.          ------------"
