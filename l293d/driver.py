e32ad8d24d957fde63aa60c04453917c3038b6e7
Alex Ward <alxwrd@googlemail.com>
2018-01-16 20:07:53 +0000

    start of pwm support

--- l293d/driver.py
+++ l293d/driver.py
@@ -62,6 +62,8 @@ class DC(object):
         self.motor_pins[1] = pin_b
         self.motor_pins[2] = pin_c
 
+        self.pwm = None
+
         self.pin_numbering = Config.pin_numbering
 
         self.reversed = False
@@ -83,7 +85,7 @@ class DC(object):
             if not Config.test_mode:
                 GPIO.setup(pin, GPIO.OUT)
 
-    def drive_motor(self, direction=1, duration=None, wait=True):
+    def drive_motor(self, direction=1, duration=None, wait=True, speed=100):
         """
         Method called by other functions to drive L293D via GPIO
         """
@@ -92,14 +94,16 @@ class DC(object):
             direction *= -1
         if not Config.test_mode:
             if direction == 0:  # Then stop motor
-                GPIO.output(self.motor_pins[0], GPIO.LOW)
+                self.pwm.stop()
             else:  # Spin motor
+                self.pwm = GPIO.PWM(self.motor_pins[0], speed)
+                self.pwm.start(0)
                 # Set first direction GPIO level
-                GPIO.output(self.motor_pins[direction], GPIO.HIGH)
+                GPIO.output(self.motor_pins[direction], True)
                 # Set second direction GPIO level
-                GPIO.output(self.motor_pins[direction * -1], GPIO.LOW)
+                GPIO.output(self.motor_pins[direction * -1], False)
                 # Turn the motor on
-                GPIO.output(self.motor_pins[0], GPIO.HIGH)
+                self.pwm.ChangeDutyCycle(speed)
         # If duration has been specified, sleep then stop
         if duration is not None and direction != 0:
             stop_thread = Thread(target=self.stop, args=(duration,))
@@ -115,7 +119,7 @@ class DC(object):
         """
         return '[{}, {} and {}]'.format(*self.motor_pins)
 
-    def __move_motor(self, direction, duration, wait, action):
+    def __move_motor(self, direction, duration, wait, action, speed):
         """
         Uses drive_motor to spin the motor in `direction`
         """
@@ -126,21 +130,22 @@ class DC(object):
                         action=action,
                         reversed='reversed' if self.reversed else '',
                         pin_nums=self.pin_numbering,
-                        pin_str=self.pins_string_list))
+                        pin_str=self.pins_string_list()))
 
-        self.drive_motor(direction=direction, duration=duration, wait=wait)
+        self.drive_motor(direction=direction, duration=duration,
+                         wait=wait, speed=speed)
 
     def clockwise(self, duration=None, wait=True, speed=100):
         """
         Spin the motor clockwise
         """
-        self.__move_motor(1, duration, wait, 'spinning clockwise')
+        self.__move_motor(1, duration, wait, 'spinning clockwise', speed)
 
     def anticlockwise(self, duration=None, wait=True, speed=100):
         """
         Spin the motor anticlockwise
         """
-        self.__move_motor(-1, duration, wait, 'spinning anticlockwise')
+        self.__move_motor(-1, duration, wait, 'spinning anticlockwise', speed)
 
     def stop(self, after=0):
         """
@@ -148,7 +153,7 @@ class DC(object):
         """
         if after > 0:
             sleep(after)
-        self.__move_motor(0, after, True, 'stopping')
+        self.__move_motor(0, after, True, 'stopping', None)
 
     def remove(self):
         """
