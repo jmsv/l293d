37594e2b6ddcfb681a21c430a06a22417271189d
James Vickery <dev@jamesvickery.net>
2018-05-07 07:09:24 +0100

    Updated setup.py, closed #88 and closed #89

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
-import io
 import re
+from codecs import open
+from os import path
 from setuptools import setup
 
-with io.open('l293d/__init__.py', 'rt', encoding='utf8') as f:
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    readme = f.read()
+
+with open(path.join(here, 'l293d/__init__.py'), encoding='utf8') as f:
     version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
 
 repo = 'https://github.com/jamesevickery/l293d'
@@ -16,27 +22,28 @@ setup(
     author_email='dev@jamesvickery.net',
     description=('A Python module to drive motors using '
                  'an L293D via Raspberry Pi GPIO'),
+    long_description=readme,
     license='MIT',
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux'
     ],
-    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, <4',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.2.*, <4',
     keywords=['raspberry', 'pi', 'gpio', 'l293d', 'chip', 'motor', 'driver'],
     url=repo,
+    project_urls={
+        'Bug Reports': '%s/issues' % repo,
+        'Source': repo
+    },
     download_url='%s/archive/v%s.tar.gz' % (repo, version),
-    entry_points={
-        'console_scripts': [
-            'sample=sample:main',
-        ],
-    }
 )
