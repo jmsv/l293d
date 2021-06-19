6fb54d02fed585f12fe03259a60e4c1ec68cdee7
James Vickery <dev@jamesvickery.net>
2021-06-19 21:19:56 +0100

    fix repo url in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 with open(path.join(here, 'l293d/__init__.py'), encoding='utf8') as f:
     version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
 
-repo = 'https://github.com/jamesevickery/l293d'
+repo = 'https://github.com/jmsv/l293d'
 
 setup(
     name='l293d',
