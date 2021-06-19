7a1529b27f7fb17dea46fbe0bd1947861a537d8d
James Vickery <dev@jamesvickery.net>
2021-06-19 21:19:37 +0100

    add latest python version classifiers to setup.py

--- setup.py
+++ setup.py
@@ -35,6 +35,9 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux'
